7459ec95b0db6107fbcb707523055499fe339d45
Kriw-Kriw <krivkriv36@gmail.com>
2024-04-24 10:24:26 +0300

    DFS was modified

--- dfs.py
+++ dfs.py
@@ -1,22 +1,27 @@
-def dfs(graph, start):
+def dfs_with_length_and_path(graph, start, end):
     visited = set()
-    stack = [start]
-    path = []
+    stack = [(start, 0, [start])]
     while stack:
-        vertex = stack.pop()
-        if vertex not in visited:
-            visited.add(vertex)
-            path.append(vertex)
-            stack.extend(graph[vertex] - visited)
-    return path
+        vertex, length, path = stack.pop()
+    if vertex == end:
+        return length, path
+    if vertex not in visited:
+        visited.add(vertex)
+        for neighbor in graph[vertex] - visited:
+            new_path = path + [neighbor]
+            stack.append((neighbor, length + 1, new_path))
+    return -1, []
 
 
-# Пример использования
 graph = {
     1: {3},
-    2: {4},
-    3: {1},
-    4: {2}
+    2: {1, 4},
+    3: {2},
+    4: {2, 3}
 }
+
 start_vertex = 1
-print(dfs(graph, start_vertex))
+end_vertex = 3
+length, path = dfs_with_length_and_path(graph, start_vertex, end_vertex)
+print("Length of the shortest path:", length)
+print("Shortest path:", path)
